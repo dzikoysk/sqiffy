f6f23dc7b4767c1707fa7cf53086ca5ce580256d
dzikoysk <dzikoysk@dzikoysk.net>
2023-04-08 01:56:43 +0200

    Release 1.0.0-alpha.12

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ allprojects {
     apply(plugin = "signing")
 
     group = "com.dzikoysk.sqiffy"
-    version = "1.0.0-alpha.11"
+    version = "1.0.0-alpha.12"
 
     java {
         sourceCompatibility = JavaVersion.VERSION_1_8
