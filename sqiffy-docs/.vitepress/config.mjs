a2fb0450f0f4be0b37461f521e97b56b2f75bbbb
dzikoysk <dzikoysk@dzikoysk.net>
2024-02-11 21:18:50 +0100

    GH-71 Fix dead link v2

--- sqiffy-docs/.vitepress/config.mjs
+++ sqiffy-docs/.vitepress/config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Docs', link: '/docs' }
+      { text: 'Docs', link: '/installation/gradle' }
     ],
 
     sidebar: [
